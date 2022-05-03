b0e3829c04a01d94bc0a36f094e4f73df36dcf6a
Utsav Roy <utsav7209543464@gmail.com>
2022-05-04 00:06:12 +0530

    querry send

--- index.php
+++ index.php
@@ -164,9 +164,9 @@ if (isset($_GET['msg'])) {
                                     <p class="heading-text">Reach out to us for any query!</p>
                                 </div>
 
-                                <div class="seo-score-form">
+                                <div class="">
 
-                                    <form class="seo-score-form input-inline crumina-submit" data-nonce="crumina-submit-form-nonce" data-type="standard" action="modules/forms/submit.php">
+                                    <form class="" data-nonce="crumina-submit-form-nonce" data-type="standard" action="querry.php" method='POST'>
 
                                         <div class="row">
 
@@ -178,7 +178,7 @@ if (isset($_GET['msg'])) {
                                         </div>
 
                                         <button class="btn btn-medium btn--green btn-hover-shadow">
-                                            <span class="text">Check Up!</span>
+                                            <span class="text">Send Us!</span>
                                             <span class="semicircle"></span>
                                         </button>
 
