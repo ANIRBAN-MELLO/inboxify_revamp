ae334f5dccb07437279127084c587b3fdf846167
ANIRBAN-MELLO <anirbanbhowmick11@gmail.com>
2022-05-03 23:18:14 +0530

    updated the signup code

--- signupProcess.php
+++ signupProcess.php
@@ -1,25 +1,28 @@
 <?php include("includes/dbcon.php"); 
 
-$fname = $_POST['fname'];
-$lname = $_POST['pass'];
-$email = $_POST['email'];
-$phone = $_POST['phone'];
-$pwd = $_POST['pwd'];
+session_start();
+
+if(isset($_POST['signup'])){
+     $fname = $_POST['fname'];
+     $lname = $_POST['lname'];
+     $email = $_POST['email'];
+     $phone = $_POST['phone'];
+     $pwd = $_POST['pwd'];
+}
 
 
-echo $fname;
 
      //$query = "SELECT `id` FROM `admin` WHERE `username` = '{$id}' and `password`='{$pass}'";
-    $query = "insert into user_data (fname,lname,email,phone,pwd) values ('{$fname}','{$lname}','{$email}','{$phone}','{$pwd}') ";
+    $query = "insert into user_data (fname,lname,email,phone,pwd) values ('{$fname}','{$lname}','{$email}',{$phone},'{$pwd}') ";
     //$query = "insert into user_data (email) values ('{$email}') ";
     $resultDomain = mysqli_query($con, $query);
     
     if (!$resultDomain) 
     {
-         die("Validation Failed please check dbcon! ->" . mysqli_error($con));
+         die("User data not saved, please check code ->" . mysqli_error($con));
     } 
-    //else {
-    //     $data = mysqli_fetch_assoc($resultDomain);
-    //     $val = $data['id'];
-
-?>
+   else{   
+     $_SESSION['id']  = mysqli_insert_id($con);
+     $_SESSION['name'] = ucfirst($fname);
+     header('location:index.php?msg=Thank you!');
+   }
